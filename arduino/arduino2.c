0883cf4d6cc3b6b161e9172787f5a63138c600ea
manumazu <emmanuel.mazurier@gmail.com>
2019-10-13 16:51:52 +0200

    map analog

--- arduino/arduino2.c
+++ arduino/arduino2.c
@@ -19,12 +19,12 @@ int blueColor = 0;
 
 const int buttonPin = 4;
 int buttonState = 1;
-int stateReset = 2;
+int stateReset = 0;
 
-int sensorCols = 0;//for columns
+//for columns
 int outputCols = 0;
 int currentCol = 0;
-int sensorRow = 0;//for lines
+//for lines
 int outputRow = 0;
 int currentRow = 0;
 
@@ -41,27 +41,25 @@ void setup() {
   pinMode(A0, INPUT);//col
   pinMode(A1, INPUT);//row  
   Serial.begin(9600);
-  setColor(); 
+  setColor();
 }
 
 void getLedAddress() {
-  sensorCols = analogRead(A0);
   // map it to the range pix of the analog out:
-  outputCols = map(sensorCols, 0, 1023, 0, 11);
+  outputCols = map(analogRead(A0), 0, 1024, 0, NUMPIXELS1);
   
-  sensorRow = analogRead(A1);
   // map it to the range lines of the analog out:
-  outputRow = map(sensorRow, 0, 1023, 1, 2);  
+  outputRow = map(analogRead(A1), 0, 1024, 1, 3);  
  
   address[0]=outputCols;//pos
   address[1]=outputRow;//line
-  address[2]=2;//space
+  address[2]=1;//space
   
   Serial.println(address[0]+(String)'-'+address[1]);
 }
 
 void ledsManager(String action, int pos = 0, int line = 0, int space = 0) {
-  
+   
    for (int j=1; j <= 2; j++) 
    {
       if (j==1) {
@@ -82,30 +80,28 @@ void ledsManager(String action, int pos = 0, int line = 0, int space = 0) {
          currentPix.setPixelColor(i, currentPix.Color(255, 69, 0));
        }   
        if(action.equals("on")){
-         if(line==j && i>=pos && i<=(pos+space-1)) {//light on given line
+         if(line==j && i>=pos && i<=(pos+space-1)) //light on given line
         	currentPix.setPixelColor(i, currentPix.Color(redColor, greenColor, blueColor));
             currentCol = pos;
             currentRow = line;
             currentadd[0]=pos;
             currentadd[1]=line;
             currentadd[2]=space;
-         }
        }
        currentPix.show();
      }
-
    } 
 }
 
 
 void loop() {
-  
+
   getLedAddress();
   
   if(outputCols!=currentCol || outputRow!=currentRow) {// new request
     stateReset=1;
     ledsManager(String("off"));//switch off all
-  }   
+  }
   
   //light asked led  
   if(stateReset==1) { 
