c511047093ec8c491226e769ec2a3dd75f23ffbf
ManuMazu <emmanuel.mazurier@gmail.com>
2019-10-29 23:47:03 +0100

    debug sorting after drag&drop

--- biblioapp/static/range.js
+++ biblioapp/static/range.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var selectedClass = 'ui-state-highlight',
+    /*var selectedClass = 'ui-state-highlight', 
         clickDelay = 600,
         // click time (milliseconds)
         lastClick, diffClick; // timestamps
@@ -43,6 +43,7 @@ $(document).ready(function() {
     });
 
     $("#droppable, #draggable").sortable().droppable({
+        hoverClass: "ui-state-hover",
         drop: function(e, ui) {
             $('.' + selectedClass).appendTo($(this)).add(ui.draggable) // ui.draggable is appended by the script, so add it after
             .removeClass(selectedClass).css({
@@ -51,12 +52,52 @@ $(document).ready(function() {
             });
         }
     });
+   //$("#droppable").disableSelection();*/
 
-   $("#droppable").sortable({	        
+   var draggableElementsSize1 = $('#draggable li').size(), selectedClass = 'ui-state-default';
+   var i1 = 0;
+
+   $("#droppable").droppable({
+      scope: "d1",
+      activeClass: "ui-state-default",
+      hoverClass: "ui-state-hover",
+      accept: '#draggable li',
+      drop: function( event, ui ) {
+        i1++;
+        ui.draggable.draggable('option','revert',false); 
+	ui.draggable.clone().appendTo(this);
+        ui.draggable.remove();
+     }
+     }).sortable({
+      items: "li",
+      sort: function() {
+        $( this ).removeClass("ui-state-default");
+      }
+   });
+
+   $('#draggable li').draggable({
+    appendTo: "body",
+    //helper: "clone",
+    revert: true,
+    scope: "d1",
+    cursor: 'move',
+    start: function(e, ui) {
+       ui.helper.addClass(selectedClass);
+    },
+    stop: function(e, ui) {
+      // reset group positions
+      $('.' + selectedClass).css({
+        top: 0,
+        left: 0
+      });
+    }
+   });
+
+   $("#droppable").sortable({
 	update: function(e, ui) {
 	// POST to server using $.ajax
 	var order = $('#droppable').sortable('serialize'); 
-	console.log(order2;
+	console.log(order);
 	/*$.ajax({
 	    data: order,
 	    type: 'POST',
