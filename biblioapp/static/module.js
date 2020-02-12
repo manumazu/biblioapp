d57874362005e0df55f45b6bfd0219da4e7e9b7a
ManuMazu <emmanuel.mazurier@gmail.com>
2020-02-12 23:31:25 +0100

    manage sliders and multi handlers

--- biblioapp/static/module.js
+++ biblioapp/static/module.js
@@ -1,21 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function() {	
 
-	var setSlider = function ( values ) {
-		$("#slider").slider({
+	$(sliderList).each(function(i, static) {
+    	var handler = [];
+		$("#"+static+" li").each( function(index) {
+			var val = $(this).text();
+			if($.isNumeric(val))
+				handler.push(val);
+		});
+
+		//$("#add_"+(i+1)).click(function(){handler.push(40);});
+		$("#add_"+(i+1)).on('click', function(){
+			handler.push("40");
+			setSlider((i+1), handler);
+		});
+
+		setSlider((i+1), handler);
+ 	});
+
+
+
+	function setSlider ( index, values ) {
+		console.log(values);	
+		$("#slider_"+index).slider({
 		  values: values,
 		  change: function( event, ui ) {
 		  	console.log(ui.values);
 		  }
 		});
-	};
-	
-	var handler = [];
-	$("#static li").each( function(index) {
-		var val = $(this).text();
-		if($.isNumeric(val))
-			handler.push(val);
-	});
-
-	setSlider(handler);
+	}
 
 });
