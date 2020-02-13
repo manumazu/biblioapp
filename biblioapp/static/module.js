a6845fd44921cac17cc32997140ca4e5270ba871
Bibliobus <bibliobus@koda.bearstech.com>
2020-02-13 19:19:04 +0100

    manage multi handlers for sliders

--- biblioapp/static/module.js
+++ biblioapp/static/module.js
@@ -1,32 +1,44 @@
 $(document).ready(function() {	
 
 	$(sliderList).each(function(i, static) {
-    	var handler = [];
+    		var handler = [];
+		i++;
 		$("#"+static+" li").each( function(index) {
 			var val = $(this).text();
 			if($.isNumeric(val))
 				handler.push(val);
 		});
 
-		//$("#add_"+(i+1)).click(function(){handler.push(40);});
-		$("#add_"+(i+1)).on('click', function(){
-			handler.push("40");
-			setSlider((i+1), handler);
+		
+		$("#add_"+i).on('click', function(){
+			handler.push("0");
+			$("#slider_"+i).slider("destroy");
+			setSlider(i, handler, static);
 		});
 
-		setSlider((i+1), handler);
+		setSlider(i, handler, static);
  	});
 
 
-
-	function setSlider ( index, values ) {
-		console.log(values);	
+	function setSlider ( index, values, static ) {
+		//console.log(values);	
 		$("#slider_"+index).slider({
+		  min: 0,
+		  max: 61,
 		  values: values,
 		  change: function( event, ui ) {
-		  	console.log(ui.values);
+		  	//console.log(ui.values);
+			showSliderVal(static, ui.values);
 		  }
 		});
+	}
+
+	function showSliderVal(static, values) {
+		for(i=0;i<values.length;i++) {
+			console.log(values[i]);
+			if($("#"+static).length)
+				$("#"+static).append('<li>'+values[i]+'<li>');
+		}
 	}
 
 });
