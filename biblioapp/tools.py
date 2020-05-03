a1c0715207611800c7ef2e88e9fccf96db8492f1
ManuMazu <emmanuel.mazurier@gmail.com>
2020-05-03 19:19:37 +0200

    remove debug

--- biblioapp/tools.py
+++ biblioapp/tools.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from biblioapp import app, hashlib, base64
 
 def getYear(datestr):
-  print(datestr)
   try:
     datepub = datetime.strptime(datestr,'%b %d, %Y')
     return datepub.year
