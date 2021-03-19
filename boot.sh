e4d9515033ceab2d30fafa60bedb3337ce4972c1
ManuMazu <emmanuel.mazurier@gmail.com>
2021-03-19 23:25:25 +0100

    #37 debug boot args for gunicorn

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 . venv/bin/activate
-exec gunicorn --bind :5000 --workers 4 --access-logfile - --error-logfile - --capture-output --reload-engine biblioapp:app
+exec gunicorn --bind :5000 --workers 4 --access-logfile - biblioapp:app
