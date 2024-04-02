defa50a1fdb097e87ec4efda7f9817c6f9e3fa67
Emmanuel Mazurier <emmanuel.mazurier@gmail.com>
2024-04-02 22:51:04 +0200

    prepare save book position for books found

--- biblioapp/static/ocr.js
+++ biblioapp/static/ocr.js
@@ -1,60 +1,59 @@
 $(document).ready(function() {
 
-$('#start-ocr').on('click', function() {
-    $(this).text("Indexation in process");
-    $(this).addClass('btn-warning');
-    $('#ocrResult').empty();
+  $('#start-ocr').on('click', function() {
+      $(this).text("Indexation in process");
+      $(this).addClass('btn-warning');
+      $('#ocrResult').empty();
 
-    const timer = waitingButton(this, 20);
-    var numshelf = $('#numshelf').val();
-    var button = this;
-    
-    $(':checkbox:checked').each(async function(){
+      const timer = waitingButton(this, 20);
+      var numshelf = $('#numshelf').val();
+      var button = this;
+      
+      $(':checkbox:checked').each(async function(){
 
-        const filename = $(this).val();
-        const img_num = filename.split('_');
+          const filename = $(this).val();
+          const img_num = filename.split('_');
 
-        // prepare display results
-        const list_header = '<hr><h2>Analyze result for image ' + filename + '</h2><ul class="list-group" style="color:#000"></ul>';
-        $('#ocrResult').append('<div id="ocrResultFound_' + img_num[0] + '">' + list_header + '</div>');
-        $('#ocrResultFound_' + img_num[0]).hide();
+          // prepare display results
+          const list_header = '<hr><h2>Analyze result for image ' + filename + '</h2><ul class="list-group" style="color:#000"></ul>';
+          $('#ocrResult').append('<div id="ocrResultFound_' + img_num[0] + '">' + list_header + '</div>');
+          $('#ocrResultFound_' + img_num[0]).hide();
 
-        //start ocr
-        const params = 'img='+filename+'&numshelf='+numshelf+'&img_num='+img_num[0];
-        const ocr = await ajax_postOcr(params, timer);
-        //console.log(ocr); 
+          //start ocr
+          const params = 'img='+filename+'&numshelf='+numshelf+'&img_num='+img_num[0];
+          const ocr = await ajax_postOcr(params, timer);
+          //console.log(ocr); 
 
-        if(ocr.success == true) 
-        {
-          $('#ocrResultFound_' + ocr.img_num).show();
-          // use search book api for each ocr result
-          const books = ocr.response;
-          for(let j = 0; j<books.length; j++) 
+          if(ocr.success == true) 
           {
-            let result = await searchBook(books[j], (j+1))
-            //console.log(result)
-            $('#ocrResultFound_' + ocr.img_num + ' ul').append(result);
+            $('#ocrResultFound_' + ocr.img_num).show();
+            // use search book api for each ocr result
+            const books = ocr.response;
+            for(let j = 0; j<books.length; j++) 
+            {
+              let result = await ajax_searchBook(books[j], (j+1))
+              //console.log(result)
+              $('#ocrResultFound_' + ocr.img_num + ' ul').append(result);
+            }
+            //display ocr analyse total found
+            console.log("ocr books found:", ocr.ocr_nb_books);           
           }
-          //display ocr analyse total found
-          console.log("ocr books found:", ocr.ocr_nb_books);           
-        }
-        else {
-          $('#ocrResult').append('<div class="error"><hr><h2>OCR error for image ' + filename + '</h2><p>' + ocr.response + '</p></div>');
-        }
+          else {
+            $('#ocrResult').append('<div class="error"><hr><h2>OCR error for image ' + filename + '</h2><p>' + ocr.response + '</p></div>');
+          }
 
-        $(button).text("Start indexation");
-        $(button).removeClass('btn-warning');
-        clearInterval(timer);           
+          $(button).text("Start indexation");
+          $(button).removeClass('btn-warning');
+          clearInterval(timer);           
     });
-
   });
 });
 
 async function ajax_postOcr(params, timer) {
 
- console.log(params);
+  console.log(params);
 
- return $.ajax({
+  return $.ajax({
     data: params,
     type: 'POST',
     url: '/api/ajax_ocr/',
@@ -70,14 +69,14 @@ async function ajax_postOcr(params, timer) {
         clearInterval(timer);
       }
     },
-    complete: async function(res) {
+    complete: function(res) {
       return res.responseText;
     }
   });
 }
 
 // request for search api using ocr result
-async function searchBook(ocr, index) {
+async function ajax_searchBook(ocr, index) {
   var elem = [];
   //console.log(ocr)
   elem.push('title='+ocr.title)
@@ -85,7 +84,7 @@ async function searchBook(ocr, index) {
   elem.push('editor='+ocr.editor)
   elem.push('numbook='+index)
   params = elem.join('&');
- return $.ajax({
+  return $.ajax({
     data: params,
     type: 'POST',
     url: '/api/ajax_bookindexer/',
@@ -105,5 +104,18 @@ function waitingButton(button, seconds) {
         str = '.';
     }
     return setInterval(run, 500);
+}
+
+function saveBook(form_id) {
+  //prevent submit form
+  $(this).on('click', function(e){
+    e.preventDefault();
+  });
+  const reference = $('#' + form_id + ' > form > input[name=reference]').val()
+  const elem = [];
+  elem.push('ref='+reference)
+  elem.push('save_bookapi=googleapis')
+  params = elem.join('&');
+  console.log(params);
 }
 
